52f3d5afe72651dc07eb8b20fe20f38b94b72cc6
Nellyjohn <okpala_vivian@.com>
2023-12-02 07:42:11 -0800

    My program prints a matrix of integers.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for num in matrix:
         for element in num:
-            print(element, end=" ")
+            print("{}".format(element), end=" ")
         print()
