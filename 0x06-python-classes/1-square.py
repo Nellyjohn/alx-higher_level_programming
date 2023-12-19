c1ddd2d89dcb20a2a1b88f404525afd90ae911e6
Nellyjohn <okpala_vivian@.com>
2023-12-19 11:08:51 -0800

    My program is a class Square that defines a square by private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Auth: Vivian Okaforcha
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize a square with a given size
+        Args:
+            size: The size of the square
+        """
+
+        self.__size = size
