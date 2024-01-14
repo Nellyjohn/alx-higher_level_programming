4058594c0bd0eafb26f84725745ce063a4b9d6de
Nellyjohn <okpala_vivian@.com>
2024-01-14 08:38:25 -0800

    tests and update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,7 +73,36 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-
     def area(self):
+        """calculates area of rectangle"""
         result = self.height * self.width
         return (result)
+
+    def display(self):
+        """prints # to stdout"""
+        for i in range(self.y):
+            print()
+        for i in range(self.height):
+            for i in range(self.x):
+                print(" ", end="")
+            for i in range(self.width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """Represents objects as a string"""
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height))
+
+    def update(self, *args, **kwargs):
+        if args:
+            self.id = args[0] if len(args) > 0 else self.id
+            self.width = args[1] if len(args) > 1 else self.width
+            self.height = args[2] if len(args) > 2 else self.height
+            self.x = args[3] if len(args) > 3 else self.x
+            self.y = args[4] if len(args) > 4 else self.y
+        elif kwargs:
+            self.id = kwargs.get('id', self.id)
+            self.width = kwargs.get('width', self.width)
+            self.height = kwargs.get('height', self.height)
+            self.x = kwargs.get('x', self.x)
+            self.y = kwargs.get('y', self.y)
