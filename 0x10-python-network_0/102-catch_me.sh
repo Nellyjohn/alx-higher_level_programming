d13af12a8c09d051d172743438c5b0b36166a058
veeviyan <okpala_vivian@yahoo.com>
2024-04-07 09:38:30 +0100

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-
-curl 0.0.0.0:5000/catch_me
-printf "You got me!\n"
+curl -sL -d "user_id=98" "0.0.0.0:5000/catch_me" -H "Origin:HolbertonSchool" -X PUT 
